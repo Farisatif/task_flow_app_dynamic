181ecf7ea89280cb12a0a2f75ea05070c90d216a
彡ℤ𝕖𝕣𝕖𝕗 <146383261+Farisatif@users.noreply.github.com>
2026-07-30 00:39:48 +0300

    Implement reset method in SettingsProvider
    Added a reset method to restore default settings.

--- lib/core/utils/settings_provider.dart
+++ lib/core/utils/settings_provider.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
 
 class SettingsProvider extends ChangeNotifier {
+  static const Color _defaultPrimaryColor = Color(0xFF7B6FF0);
+
   bool _soundEnabled = true;
   bool _hapticEnabled = true;
-  Color _primaryColor = const Color(0xFF7B6FF0);
+  Color _primaryColor = _defaultPrimaryColor;
 
   bool get soundEnabled => _soundEnabled;
   bool get hapticEnabled => _hapticEnabled;
   Color get primaryColor => _primaryColor;
 
   void setSoundEnabled(bool value) {
+    if (_soundEnabled == value) return;
     _soundEnabled = value;
     notifyListeners();
   }
 
   void setHapticEnabled(bool value) {
+    if (_hapticEnabled == value) return;
     _hapticEnabled = value;
     notifyListeners();
   }
 
   void setPrimaryColor(Color color) {
+    if (_primaryColor.value == color.value) return;
     _primaryColor = color;
+    notifyListeners();
+  }
+
+  void reset() {
+    _soundEnabled = true;
+    _hapticEnabled = true;
+    _primaryColor = _defaultPrimaryColor;
     notifyListeners();
   }
 }
