34684e0578b831a4f729f54dfdb17f516f372272
彡ℤ𝕖𝕣𝕖𝕗 <146383261+Farisatif@users.noreply.github.com>
2026-07-30 02:44:13 +0300

    Refactor build.gradle.kts for improved structure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,40 +1,32 @@
-plugins {
-    id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+import org.gradle.api.tasks.compile.JavaCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
-android {
-    namespace = "com.example.task_flow_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+val newBuildDir = rootProject.layout.buildDirectory.dir("../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
-    compileOptions {
+subprojects {
+    val subprojectBuildDir = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(subprojectBuildDir)
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions.jvmTarget = "11"
+    }
+    tasks.withType<JavaCompile>().configureEach {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
-    defaultConfig {
-        applicationId = "com.example.task_flow_app"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
 }
 
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
-}
-
-flutter {
-    source = "../.."
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
